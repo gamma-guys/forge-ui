20c599174a1c2129a93507a8cdbe7515eeebf151
Liam Davis <58375683+dayvista@users.noreply.github.com>
2021-04-18 14:52:59 -0400

    Create npm package template for future projects (#2)
    * complete npm package template
    
    * create darkmodeswitch
    
    * feat(stitchesconfig.ts): create stitches config with theming, utils, breakpoints, etc
    
    * fix(rollup.config.js): fix module imports throwing type error when `npm run build` is run
    
    * feat(stitches.config.ts && components): configure and export proper stitches / dark theme configs
    
    Also make further progress on DarkThemeSwitch components
    
    * docs(readme.md): initialize readme
    
    * refactor(forgeuiprovider.tsx): complete implementation of globalstyles
    
    integrate globalstyles into forgeuiprovider for more unified approach on client side
    
    * replace stitches/radix with chakra ui
    
    * refactor(app): convert to example for future attempts at creating npm packages
    
    * chore(release): 0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# forge-ui
+# Forge UI
+
+## Component library for [Forge](https://github.com/gamma-guys/forge) by [Gamma Guys Studio](https://github.com/gamma-guys)
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-const Package: React.FC = () => (
-  <div className="package">
-    <h2>Do cool stuff</h2>
-  </div>
-);
-
-export default Package;
