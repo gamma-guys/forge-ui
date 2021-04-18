a52f3ca038f1cfab5a8ea80220e2f0e6103b1c76
William Davis <liamdavis@tuta.io>
2021-04-18 12:47:26 -0600

    replace stitches/radix with chakra ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,1 @@
-import ForgeUiProvider from "src/components/ForgeUiProvider";
-import SsrThemeConfig from "src/components/SsrThemeConfig";
-import DarkThemeSwitch from "src/components/switches/DarkThemeSwitch";
-
-export { ForgeUiProvider, SsrThemeConfig };
-export { DarkThemeSwitch };
+export {};
